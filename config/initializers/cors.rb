0ea2de41b43c0f5cfeda4d785bd49bafb79d3785
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2022-12-08 17:17:04 +0900

    Change the domain to my localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://test-pf-vue.vercel.app'
+    origins 'http://localhost:8081'
 
     resource '*', headers: :any, methods: %i[post options head]
   end
